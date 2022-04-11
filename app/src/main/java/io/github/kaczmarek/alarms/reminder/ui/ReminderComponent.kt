cdc1cad94637d293d5c7ceadfe8ff7352291ce8e
Angelina Podbolotova <a.podbolotova@mintmail.ru>
2022-04-11 21:46:05 +0300

    added reminder screen

--- app/src/main/java/io/github/kaczmarek/alarms/reminder/ui/ReminderComponent.kt
+++ app/src/main/java/io/github/kaczmarek/alarms/reminder/ui/ReminderComponent.kt
@@ -1,3 +1,16 @@
 package io.github.kaczmarek.alarms.reminder.ui
 
-interface ReminderComponent
+import me.aartikov.sesame.compose.form.control.InputControl
+
+interface ReminderComponent {
+
+    val titleInput: InputControl
+
+    val descriptionInput: InputControl
+
+    val buttonsEnabled: Boolean
+
+    fun onWorkManagerClick()
+
+    fun onAlarmManagerClick()
+}
