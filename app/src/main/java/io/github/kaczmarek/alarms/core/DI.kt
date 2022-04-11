fc41bf96dd8c60bf85b1ecbd3de4e331d08f9788
Angelina Podbolotova <a.podbolotova@mintmail.ru>
2022-04-11 18:38:03 +0300

    created app wireframe

--- app/src/main/java/io/github/kaczmarek/alarms/core/DI.kt
+++ app/src/main/java/io/github/kaczmarek/alarms/core/DI.kt
@@ -1,5 +1,6 @@
 package io.github.kaczmarek.alarms.core
 
+import io.github.kaczmarek.alarms.core.error_handling.ErrorHandler
 import io.github.kaczmarek.alarms.core.message.data.MessageService
 import io.github.kaczmarek.alarms.core.message.data.MessageServiceImpl
 import org.koin.dsl.module
@@ -7,4 +8,5 @@ import org.koin.dsl.module
 val coreModule = module {
     single { ActivityProvider() }
     single<MessageService> { MessageServiceImpl() }
+    single { ErrorHandler(get()) }
 }
